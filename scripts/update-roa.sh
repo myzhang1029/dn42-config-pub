8b8ab20e92987eca811ad3d66fde5042d7fe4510
Zhang Maiyun <me@maiyun.me>
2025-09-21 22:31:23 -0400

    More aggressive check

--- scripts/update-roa.sh
+++ scripts/update-roa.sh
@@ -12,8 +12,10 @@ dl() {
         exit "$e"
     }
     [ -f "$t" ] || return 0
+    diff "$t" "$2" > /dev/null
+    d=$?
     mv "$t" "$2"
-    return 1
+    return $d
 }
 
 dl https://dn42.burble.com/roa/dn42_roa_bird2_4.conf /etc/bird/roa_dn42_v4.conf
@@ -21,4 +23,6 @@ v4_updated=$?
 dl https://dn42.burble.com/roa/dn42_roa_bird2_6.conf /etc/bird/roa_dn42_v6.conf
 v6_updated=$?
 
-[ "$v4_updated" -eq 1 ] || [ "$v6_updated" -eq 1 ] && birdc configure
+if [ "$v4_updated" -eq 1 ] || [ "$v6_updated" -eq 1 ]; then
+    birdc configure
+fi
