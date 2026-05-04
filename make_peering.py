2a99905a7231d8efb53d3d4aef9b23cbfa627e2e
Zhang Maiyun <me@maiyun.me>
2026-05-03 23:32:12 -0400

    Make `make_peering.py` smoother by avoiding circular data dependency with other people's AutoPeer scripts

--- make_peering.py
+++ make_peering.py
@@ -5,8 +5,10 @@ import readline as _
 from pathlib import Path
 from pprint import pprint
 
+
 class MalformedConfig(Exception):
     pass
+
 
 class MakePeer:
     # Defined in each nftables/main.nft
@@ -88,10 +90,10 @@ Peer={}
         self.ask_questions()
         print("Will generate based on the following answers:")
         pprint(self.answers)
-        self._print_additional_info()
+        self._print_additional_info(pre=False)
         resp = input("Continue? [Y/n] ").strip()
         if resp.lower() == "n":
-            print("Will not generate files.")
+            print("Will not generate files")
             return
         self._generate_files()
 
@@ -112,41 +114,63 @@ Peer={}
         return None
 
     @staticmethod
-    def _ask_numeric(question: str, can_strip_prefix: str = "") -> int:
+    def _ask_numeric(
+        question: str, can_strip_prefix: str = "", default: int | None = None
+    ) -> int:
         """Ask a numeric question."""
+        if not question.endswith(" "):
+            question += " "
+        if default is not None:
+            question += f"[default: {default}] "
         while True:
             try:
                 ans = input(question).strip()
                 if can_strip_prefix and ans.startswith(can_strip_prefix):
-                    ans = ans[len(can_strip_prefix):]
+                    ans = ans[len(can_strip_prefix) :]
                 return int(ans)
             except ValueError:
-                print("Invalid input, try again.\n")
+                print("Invalid input; try again\n")
 
     @staticmethod
-    def _ask_string(question: str) -> str:
+    def _ask_string(question: str, default: str | None = None) -> str:
         """Ask a string question."""
-        return input(question).strip()
+        if not question.endswith(" "):
+            question += " "
+        if default is not None:
+            question += f"[default: {default}] "
+        while True:
+            answer = input(question).strip()
+            if answer:
+                return answer
+            elif default is not None:
+                return default
+            print("A response is required; try again\n")
 
     @staticmethod
-    def _ask_wgkey(question: str) -> str:
+    def _ask_wgkey(question: str, default: str | None = None) -> str:
         """Ask for a WireGuard key."""
+        if not question.endswith(" "):
+            question += " "
+        if default is not None:
+            question += f"[default: {default}] "
+
         def valid_key(key: str) -> bool:
             if len(key) != 44:
                 return False
             if key[-1] != "=":
                 return False
-            chrs = set(key[:-1]) - {'+', '/'}
+            chrs = set(key[:-1]) - {"+", "/"}
             return all(c.isalnum() for c in chrs)
+
         while True:
             key = input(question).strip()
             if valid_key(key):
                 return key
-            print("Invalid key, try again.\n")
+            print("Invalid key; try again\n")
 
     @staticmethod
     def _translate_underscore(original: str) -> str:
-        tr = str.maketrans({'.':'_', '-':'_', ' ':''})
+        tr = str.maketrans({".": "_", "-": "_", " ": ""})
         return original.translate(tr)
 
     def _ask_site(self) -> str:
@@ -154,7 +178,7 @@ Peer={}
         while True:
             print("Which site is this peer for?")
             for i, name in enumerate(self.SITES):
-                print(f"{i+1}. {name}")
+                print(f"{i + 1}. {name}")
             choice = input("Answer: ").strip()
             site = self._parse_choice(self.SITES, choice)
             if site is not None:
@@ -173,15 +197,18 @@ Peer={}
                 if line.startswith(LOOKFOR):
                     if this_port is not None:
                         raise MalformedConfig(f"multiple {LOOKFOR}")
-                    this_port = int(line[len(LOOKFOR):])
+                    this_port = int(line[len(LOOKFOR) :])
             if this_port is None:
                 raise MalformedConfig(f"missing {LOOKFOR}")
             site_used_ports.add(this_port)
+        available = set(self.PORT_RANGE).difference(site_used_ports)
         print(f"Please select a listening port in {self.PORT_RANGE}")
         print("These ports are in use:")
         pprint(site_used_ports)
         while True:
-            port = self._ask_numeric("Which port should we listen on? ")
+            port = self._ask_numeric(
+                "Which port should we listen on?", default=list(available)[0]
+            )
             if port not in site_used_ports and port in self.PORT_RANGE:
                 return port
             print("This port is unavailable.\n")
@@ -189,57 +216,72 @@ Peer={}
     def ask_questions(self) -> None:
         """Ask all questions."""
         self.answers["site"] = self._ask_site()
-        self.answers["asn"] = str(self._ask_numeric("What is the peer ASN? ", "AS"))
-        self.answers["pname"] = self._ask_string(
-            "What is a descriptive name for the peer? ")
-        self.answers["ploc"] = self._ask_string(
-            "What is a descriptive location for the peer? ")
-        self._generate_names()
-        self.answers["ppub"] = self._ask_wgkey(
-            "What is the peer's public key? ")
         self.answers["listen_port"] = str(self._ask_listen_port())
-        print("Note: if the peer does not have a public endpoint, leave this blank.")
+        self._print_additional_info(pre=True)
+        print()
+        self.answers["asn"] = str(self._ask_numeric("What is the peer ASN?", "AS"))
+        self.answers["pname"] = self._ask_string(
+            "What is a descriptive name for the peer?"
+        )
+        self.answers["ploc"] = self._ask_string(
+            "What is a descriptive location for the peer?"
+        )
+        self._generate_names()
+        self.answers["ppub"] = self._ask_wgkey("What is the peer's public key?")
+        print("Note: if the peer does not have a public endpoint, leave this blank")
         self.answers["endpoint"] = self._ask_string(
-            "What is the endpoint for the peer? ")
-        print("Note: if the peer does not use IPv6LL, fill something else and manually edit the file.")
+            "What is the endpoint for the peer?"
+        )
+        print(
+            "Note: if the peer does not use IPv6LL, fill something else and manually edit the file"
+        )
         asn_lastfour = int(str(self.answers["asn"])[-4:])
         maybe = f"fe80::{asn_lastfour}"
         self.answers["peeraddr"] = self._ask_string(
-            f"What is the peer's IPv6 link-local address? [default: {maybe}] ") or maybe
-        if '/' not in self.answers["peeraddr"]:
+            f"What is the peer's IPv6 link-local address?", default=maybe
+        )
+        if "/" not in self.answers["peeraddr"]:
             self.answers["peeraddr"] += "/64"
         self.answers["ownaddr"] = self._ask_string(
-            "What is our IPv6 link-local address? [default: fe80::893] ") or "fe80::893"
-        if '/' not in self.answers["ownaddr"]:
+            "What is our IPv6 link-local address?", default="fe80::893"
+        )
+        if "/" not in self.answers["ownaddr"]:
             self.answers["ownaddr"] += "/64"
 
     def _generate_names(self) -> None:
         """Generate interface, file, and bird names."""
         # WireGuard interface name
         if len(self.answers["pname"] + self.answers["ploc"]) > 10:
-            print("Warning: names are too long, please specify a shorter name for the interface name generation.")
+            print(
+                "Warning: names are too long, please specify a shorter name for the interface name generation"
+            )
         maybe = self._translate_underscore(self.answers["pname"].lower())
         short_name = self._ask_string(
-            f"What is the short name for the peer? [default: {maybe}] ") or maybe
+            f"What is the short name for the peer?", default=maybe
+        )
         maybe = self._translate_underscore(self.answers["ploc"].lower())
         short_loc = self._ask_string(
-            f"What is the short location for the peer? [default: {maybe}] ") or maybe
-        maybe = "wg{}{}{}".format(
-            str(self.answers["asn"])[-4:], short_name, short_loc)
+            f"What is the short location for the peer?", default=maybe
+        )
+        maybe = "wg{}{}{}".format(str(self.answers["asn"])[-4:], short_name, short_loc)
         iface_name = self._ask_string(
-            f"The interface name for the peer? [default: {maybe}] ") or maybe
+            f"The interface name for the peer?", default=maybe
+        )
         # systemd-networkd and BIRD configuration file names
         maybe = "30-dn42-{}-{}".format(short_name, short_loc)
         file_name = self._ask_string(
-            f"The file name prefix for the peer? [default: {maybe}] ") or maybe
+            f"The file name prefix for the peer?", default=maybe
+        )
         # systemd-networkd description
         maybe = f"WireGuard tunnel to AS{self.answers['asn']} {self.answers['pname']} {self.answers['ploc']}"
-        systemd_desc = self._ask_string(
-            f"The description for the peer? [default: {maybe}] ") or maybe
+        systemd_desc = (
+            self._ask_string(f"The description for the peer?", default=maybe) or maybe
+        )
         # BIRD configuration name
         maybe = "{}_{}".format(short_name, short_loc)
         bird_name = self._ask_string(
-            f"The BIRD configuration name for the peer? [default: {maybe}] ") or maybe
+            f"The BIRD configuration name for the peer?", default=maybe
+        )
         # Save the names
         self.answers["iface"] = iface_name
         self.answers["file"] = file_name
@@ -251,7 +293,7 @@ Peer={}
         if file.exists():
             resp = input(f"File {file} already exists, overwrite? [y/N] ").strip()
             if resp.lower() != "y":
-                print(f"Skipping {file} generation.")
+                print(f"Skipping {file} generation")
                 return
         with open(file, "w", encoding="utf-8") as f:
             f.write(content)
@@ -260,7 +302,11 @@ Peer={}
         """Generate the BIRD configuration."""
         peerip_nocidr = self.answers["peeraddr"].split("/")[0]
         bird = self.BIRD_TEMPLATE.format(
-            self.answers["bird"], peerip_nocidr, self.answers["iface"], self.answers["asn"])
+            self.answers["bird"],
+            peerip_nocidr,
+            self.answers["iface"],
+            self.answers["asn"],
+        )
         file = self._birdconf / f"{self.answers['file']}.conf"
         self._maybe_write_file(file, bird)
 
@@ -272,7 +318,9 @@ Peer={}
             self.answers["systemd"],
             self.answers["listen_port"],
             self.answers["ppub"],
-            f"\nEndpoint={self.answers['endpoint']}" if self.answers["endpoint"] else ""
+            f"\nEndpoint={self.answers['endpoint']}"
+            if self.answers["endpoint"]
+            else "",
         )
         file = self._systemd_network / f"{self.answers['file']}.netdev"
         self._maybe_write_file(file, netdev)
@@ -284,7 +332,7 @@ Peer={}
             self.answers["iface"],
             self.answers["systemd"],
             self.answers["ownaddr"],
-            self.answers["peeraddr"]
+            self.answers["peeraddr"],
         )
         file = self._systemd_network / f"{self.answers['file']}.network"
         self._maybe_write_file(file, network)
@@ -300,41 +348,47 @@ Peer={}
             if "__MAKE_PEERING_PORT_MARKER" in line:
                 break
         else:
-            raise MalformedConfig("missing __MAKE_PEERING_PORT_MARKER in nftables/main.nft")
-        if lines[i+1].strip() != "}":
-            raise MalformedConfig("misplaced __MAKE_PEERING_PORT_MARKER in nftables/main.nft")
+            raise MalformedConfig(
+                "missing __MAKE_PEERING_PORT_MARKER in nftables/main.nft"
+            )
+        if lines[i + 1].strip() != "}":
+            raise MalformedConfig(
+                "misplaced __MAKE_PEERING_PORT_MARKER in nftables/main.nft"
+            )
         indents = lines[i].find("#")
-        new = f'{self.answers["listen_port"]},\n'
-        lines.insert(i, ' ' * indents + new)
+        new = f"{self.answers['listen_port']},\n"
+        lines.insert(i, " " * indents + new)
         with open(file, "w", encoding="utf-8") as f:
             f.writelines(lines)
-        print(f"Please manually check `{site}/nftables/main.nft` for correctness.")
+        print(f"Please manually check `{site}/nftables/main.nft` for correctness")
 
-    def _print_additional_info(self) -> None:
+    def _print_additional_info(self, pre: bool) -> None:
         """Print node information."""
         node_info = f"{self.answers['site']}.{self.DOMAIN}"
         print("\nPeering Information:")
         print(f"\tOurASN={self.OUR_ASN}")
-        print(f"\tOurIPv6={self.answers['ownaddr']}")
+        if not pre:
+            print(f"\tOurIPv6={self.answers['ownaddr']}")
         print("\tOptions=MP-BPG + Extended Next Hop")
         try:
             import dns.resolver
+
             try:
                 _ = dns.resolver.resolve(node_info, "A")
             except dns.resolver.NoAnswer:
-                print("\tWGEndpoint=This node does not have a public endpoint.")
+                print("\tWGEndpoint=This node does not have a public endpoint")
             else:
                 print(f"\tWGEndpoint={node_info}:{self.answers['listen_port']}")
             try:
                 answers = dns.resolver.resolve(node_info, "TXT")
                 for answer in answers:
                     for item in answer.strings:
-                        print('\t' + item.decode("utf-8"))
+                        print("\t" + item.decode("utf-8"))
             except dns.resolver.NoAnswer:
-                print("Could not retrieve more node information.")
+                print("Could not retrieve more node information")
         except ImportError:
-            print("Unable to automatically retrieve node information.")
-            print(f"Such information can be found by querying `TXT {node_info}`.")
+            print("Unable to automatically retrieve node information")
+            print(f"Such information can be found by querying `TXT {node_info}`")
 
     def _generate_files(self) -> None:
         """Generate the configuration files."""
