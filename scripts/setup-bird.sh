8484cf0f608b54a8c941b424df1feac6cf03ec53
Zhang Maiyun <me@maiyun.me>
2026-07-22 21:40:10 -0400

    Make setup-bird.sh rerunable

--- scripts/setup-bird.sh
+++ scripts/setup-bird.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 autoreconf -fi
 ./configure --sysconfdir=/etc --localstatedir=/var --runstatedir=/run/bird
 make -j"$(nproc)"
 sudo make install
-sudo useradd -r bird -M -d /etc/bird -s /sbin/nologin
+grep bird /etc/passwd || sudo useradd -r bird -M -d /etc/bird -s /sbin/nologin
